a0c70f59786c356b647cbcaef17c726dd7caa748
Sumanjay <cyberboysumanjay@gmail.com>
2020-03-16 19:05:21 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+#Coded by Sumanjay on 29th Feb 2020
+from flask import Flask, request, jsonify
+from inshorts import getNews
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.secret_key = "i_am_not_feeling_sleepy_so_i_am_coding_this"
+CORS(app)
+
+@app.route('/')
+def home():
+    return 'News API is UP!<br><br>A part of <a href="https://t.me/sjprojects">Sj Projects</a>'
+
+@app.route('/news')
+def news():
+    if request.method == 'GET':
+        return jsonify(getNews(request.args.get('category')))
+
+app.run(host='0.0.0.0',port=5000,use_reloader=True,debug=True)
