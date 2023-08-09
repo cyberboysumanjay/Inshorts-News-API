c6e94616c818aa6493de674e47442a46b2489f70
Sumanjay <cyberboysumanjay@gmail.com>
2023-08-09 07:13:44 +0000

    Making deta space compatible

---
+++ main.py
@@ -0,0 +1,26 @@
+#Coded by Sumanjay on 29th Feb 2020
+from flask import Flask, request, jsonify
+from inshorts import getNews
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.secret_key = "i_am_not_feeling_sleepy_so_i_am_coding_this"
+CORS(app)
+
+@app.route('/')
+def home():
+    return 'News API is UP!<br><br>A part of <a href="https://t.me/sjprojects">Sj Projects</a>'
+
+@app.route('/news')
+def news():
+    if request.method == 'GET':
+        category = request.args.get("category")
+        if not category:
+            return jsonify({
+                "error": "please add category in query params"
+            }), 404
+        return jsonify(getNews(category)), 200
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0',port=5000,use_reloader=True)
